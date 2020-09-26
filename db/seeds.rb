9ad127455135193ab1c2b413d6fcc0bece1fc97d
Asif Kamal <fisa435@Asifs-MacBook-Air.local>
2020-09-26 08:16:27 -0400

    Fixed collection_select for Sections.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Section.create(name: 'Aquarium', environment: 'Sea Water')
+Section.create(name: 'Terrain', environment: 'Rock and sand')
+Section.create(name: 'Jungle', environment: 'Humid, forested area')
+Section.create(name: 'Tundra', environment: 'Cold, frozen land')
+Section.create(name: 'Air Space', environment: 'Sparsely wooded area with open air to fly in')
+Section.create(name: 'Marshland', environment: 'Wet earth near fresh water river')
