711b95a0232295b09929b575294a03726f77a548
asif-kamal <asif.kamal1489@gmail.com>
2020-09-29 22:02:05 -0400

    Added views for ReportsController.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,19 +1,27 @@
 class ReportsController < ApplicationController
     before_action :current_user
     before_action :set_report, only: [:show, :edit, :update, :destroy]
+    before_action :get_animal
 
     def index
-        @reports = @user.reports
+        # if params[:animal_id]
+        #     @animal = Animal.find_by(id: params[:animal_id])
+        #end
+            @reports = @user.reports
+            @user = current_user
     end
     
     def new
+        # if params[:animal_id]
+        #     @animal = Animal.find_by(id: params[:animal_id])
+        # end
         @report = @user.reports.build
     end
 
     def create
         @report = @user.reports.build(report_params)
-        if @report.save
-            redirect_to user_reports_path(@report)
+        if @report.save && params[:user_id]
+            redirect_to user_reports_path
         else
             render :new
         end
@@ -23,8 +31,9 @@ class ReportsController < ApplicationController
     end
 
     def update
+
        if @report.update(report_params)
-           redirect_to user_report_path(@user)
+           redirect_to user_reports_path(@user)
         
        else
            render :edit
@@ -42,6 +51,9 @@ class ReportsController < ApplicationController
 
     private
 
+    def get_animal
+        @animal = Animal.find_by(id: params[:animal_id])
+    end
     
     def report_params
         params.require(:report).permit(:topic, :observations, :user_id, :animal_id)
