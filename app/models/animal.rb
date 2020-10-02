d4a8c66957cb39dc325ade3f45aa058fb8faf976
asif-kamal <asif.kamal1489@gmail.com>
2020-10-02 09:03:01 -0400

    Used reports partial in reports index.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,8 +3,9 @@ class Animal < ApplicationRecord
     has_many :reports
     has_many :users, through: :reports
     validates :name, presence: true
+    scope :search, -> (query) {where("name LIKE ?",  "%#{query}%")}
     
-    def self.search(query)
-        where("name like ?", "%#{query}%")
-      end
+    # def self.search(query)
+    #     where("name like ?", "%#{query}%")
+    #   end
 end
