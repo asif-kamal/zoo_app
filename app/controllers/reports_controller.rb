b2abe08fc203910febc2bff3020648b82042a7a7
asif-kamal <asif.kamal1489@gmail.com>
2020-10-02 19:51:43 -0400

    Added spec.md

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,41 +1,50 @@
 class ReportsController < ApplicationController
     before_action :current_user
     before_action :set_report, only: [:show, :edit, :update, :destroy]
-    before_action :get_animal
+    # before_action :get_animal
 
     def index
-        if params[:animal_id]
-            @reports = Animal.find_by(id: params[:animal_id]).reports
-        else
-            @reports = Report.all
+        if get_animal
+            @reports = get_animal.reports
+        
         end
              
            
     end
     
     def new
-        # if params[:animal_id]
-        #     @animal = Animal.find_by(id: params[:animal_id])
+        #byebug
+        
+        # if get_animal
+        #     @report = @animal.reports.build
         # end
         @report = @user.reports.build
     end
 
     def create
-        @report = @user.reports.build(report_params)
-        if @report.save 
-            redirect_to reports_path
-        else
-            render :new
-        end
+        # if get_animal
+        #     @report = @user.get_animal.reports.build(report_params)
+        #     if @report.save 
+        #         redirect_to animal_path(@animal)
+        #     end
+        # else
+            @report = @user.reports.build(report_params)
+                if @report.save 
+                    redirect_to reports_path
+                else
+                    render :new
+                end
+        # end
     end
 
     def edit
+
     end
 
     def update
 
        if @report.update(report_params)
-           redirect_to user_reports_path(@user)
+           redirect_to user_path(@user)
         
        else
            render :edit
@@ -49,6 +58,7 @@ class ReportsController < ApplicationController
     end
 
     def show
+        @animal = get_animal
     end
 
     private
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -4,6 +4,7 @@ class Animal < ApplicationRecord
     has_many :users, through: :reports
     validates :name, presence: true
     scope :search, -> (query) {where("name LIKE ?",  "%#{query}%")}
+    accepts_nested_attributes_for :reports
     
     # def self.search(query)
     #     where("name like ?", "%#{query}%")
